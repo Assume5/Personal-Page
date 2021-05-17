79c1352cd6e582ceb2cd9c8ae2c4d460340512df
Chenyi <38340146+Assume5@users.noreply.github.com>
2021-05-17 13:29:32 -0400

    Push

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function main(){
-    $("#skill_1_left").hide();
-    $("#skill_1_right").hide();
-    $("#skill_2_left").hide();
-    $("#skill_2_right").hide();
-    $("#skill_3_left").hide();
-    $("#skill_3_right").hide();
-    $("#skill_4_left").hide();
-    $("#skill_4_right").hide();
-    $("#skill_4_heading").hide();
-    $("#skill_4_hr").hide();
-    $("#project_content").hide();
+    $("#skill_1_left").css('visibility', 'hidden');
+    $("#skill_1_right").css('visibility', 'hidden');
+    $("#skill_2_left").css('visibility', 'hidden');
+    $("#skill_2_right").css('visibility', 'hidden');
+    $("#skill_3_left").css('visibility', 'hidden');
+    $("#skill_3_right").css('visibility', 'hidden');
+    $("#skill_4_left").css('visibility', 'hidden');
+    $("#skill_4_right").css('visibility', 'hidden');
+    $("#skill_4_heading").css('visibility', 'hidden');
+    $("#skill_4_hr").css('visibility', 'hidden');
+    $("#project_content").css('visibility', 'hidden');
     
     document.documentElement.style.setProperty('--animate-duration', '2s');
 window.addEventListener("scroll", myScrollFunc);
@@ -17,47 +17,66 @@ window.addEventListener("scroll", myScrollFunc);
 }
 var myScrollFunc = function () {
         var y = window.scrollY;
-        if (y >= 200) {
-            $("#skill_1_left").show();
-            $("#skill_1_right").show();
+        if (y >= 400) {
+            $('#skill_1_left').css('visibility', "visible");
+            $("#skill_1_right").css('visibility', 'visible');
+            document.getElementById("skill_1_left").className="col-md-6 animate__animated animate__fadeInLeft"
+            document.getElementById("skill_1_right").className='col-md-6 animate__animated animate__fadeInRight'
         }
         if (y >= 850) {
-            $("#skill_2_left").show();
-            $("#skill_2_right").show();
+            $("#skill_2_left").css('visibility', 'visible');
+            $("#skill_2_right").css('visibility', 'visible');
+            document.getElementById("skill_2_left").className="col-md-6 animate__animated animate__fadeInLeft"
+            document.getElementById("skill_2_right").className='col-md-6 animate__animated animate__fadeInRight'
         }
         if (y >= 1250) {
-            $("#skill_3_left").show();
-            $("#skill_3_right").show();
+            $("#skill_3_left").css('visibility', 'visible');
+            $("#skill_3_right").css('visibility', 'visible');
+            document.getElementById("skill_3_left").className="col-md-6 animate__animated animate__fadeInLeft"
+            document.getElementById("skill_3_right").className='col-md-6 animate__animated animate__fadeInRight'
         }
         if (y >= 2200) {
-            $("#skill_4_hr").show();
-            $("#skill_4_heading").show();
-            $("#skill_4_left").show();
-            $("#skill_4_right").show();
+            $("#skill_4_hr").css('visibility', 'visible');
+            $("#skill_4_heading").css('visibility', 'visible');
+            $("#skill_4_left").css('visibility', 'visible');
+            $("#skill_4_right").css('visibility', 'visible');
+            document.getElementById("skill_4_left").className="col-md-6 animate__animated animate__fadeInLeft"
+            document.getElementById("skill_4_right").className='col-md-6 animate__animated animate__fadeInRight'
         }
         if (y >= 2500) {
-            $("#project_content").show();
+            $("#project_content").css('visibility', 'visible');
+            document.getElementById("project_content").className="content animate__animated animate__fadeInLeft"
         }
         if (y <= 2500) {
-            $("#project_content").hide();
+            $("#project_content").css('visibility', 'hidden');
+            document.getElementById("project_content").className=""
+
         }
         if (y <= 2200) {
-            $("#skill_4_left").hide();
-            $("#skill_4_right").hide();
-            $("#skill_4_heading").hide();
-            $("#skill_4_hr").hide();
+            $("#skill_4_left").css('visibility', 'hidden');
+            $("#skill_4_right").css('visibility', 'hidden');
+            $("#skill_4_heading").css('visibility', 'hidden');
+            $("#skill_4_hr").css('visibility', 'hidden');
+            document.getElementById("skill_4_left").className=""
+            document.getElementById("skill_4_right").className=''
         }
         if (y <= 1250) {
-            $("#skill_3_left").hide();
-            $("#skill_3_right").hide();
+            $("#skill_3_left").css('visibility', 'hidden');
+            $("#skill_3_right").css('visibility', 'hidden');
+            document.getElementById("skill_3_left").className=""
+            document.getElementById("skill_3_right").className=''
         }
         if (y <= 950) {
-            $("#skill_2_left").hide();
-            $("#skill_2_right").hide();
+            $("#skill_2_left").css('visibility', 'hidden');
+            $("#skill_2_right").css('visibility', 'hidden');
+            document.getElementById("skill_2_left").className=""
+            document.getElementById("skill_2_right").className=''
         }
         if (y <= 200) {
-            $("#skill_1_left").hide();
-            $("#skill_1_right").hide();
+            $("#skill_1_left").css('visibility', 'hidden');
+            $("#skill_1_right").css('visibility', 'hidden');
+            document.getElementById("skill_1_left").className=""
+            document.getElementById("skill_1_right").className=''
         }
 }
 var isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)
